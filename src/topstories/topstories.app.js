478e7a29b3172284543f5df4849fb8311c328cb3
Brad Serbu <bradserbu@bradserbu.com>
2015-11-16 16:22:11 -0500

    Implemented getArticleContent to extract article text using API similar to readability.

--- src/topstories/topstories.app.js
+++ src/topstories/topstories.app.js
@@ -14,7 +14,13 @@ const request = require('request');
 const cheerio = require('cheerio');
 const errors = require('papyri').errors;
 const logger = require('papyri').logger;
+const readability = require('node-readability');
+const sanitizer = require('sanitizer');
 
+/**
+ * Return a list of the 'Top Stories' from the Google News page
+ * @param callback
+ */
 function getTopics(callback) {
     // ** Fetch the page contents
     request.get(GOOGLE_TOPICS, (err, response, html) => {
@@ -48,7 +54,11 @@ function getTopics(callback) {
     });
 }
 
-// ** Return a list of headlines for a specific topic
+/**
+ * Return a list of headlines for a specific topic.
+ * @param topic
+ * @param callback
+ */
 function getTopicHeadlines(topic, callback) {
 
     const url = topic.url;
@@ -69,13 +79,15 @@ function getTopicHeadlines(topic, callback) {
             const url = $(div.find('a')).attr('href');
 
             headlines.push({
-                topic: topic,
                 title: title,
                 url: url
             });
         }
 
-        callback(null, headlines);
+        callback(null, {
+            "topic": topic,
+            "headlines": headlines
+        });
     });
 }
 
@@ -92,5 +104,83 @@ function getHeadlines(callback) {
     });
 }
 
+/**
+ * Returns a list of Entities, and their count given a list of headlines for a topic
+ * @param headlines
+ * @param callback
+ */
+function getHeadlineEntities(headline, callback) {
+    callback(null, 'NOT_IMPLEMENTED');
+}
+
+function getEntities(callback) {
+    getTopics((err, topics) => {
+        if (err) return callback(err);
+
+        // ** Fetch headlines for each topic
+        //async.map(topics, getTopicHeadlines, (err, results) => {
+        //    if (err) return callback(err);
+        //
+        //    callback(null, results);
+        //});
+    });
+}
+
+function stripHTML(html) {
+    var clean = sanitizer.sanitize(html, function (str) {
+        return str;
+    });
+
+    // Remove all remaining HTML tags.
+    clean = clean.replace(/<(?:.|\n)*?>/gm, "");
+
+    // RegEx to remove needless newlines and whitespace.
+    // See: http://stackoverflow.com/questions/816085/removing-redundant-line-breaks-with-regular-expressions
+    clean = clean.replace(/(?:(?:\r\n|\r|\n)\s*){2,}/ig, "\n");
+
+    // Return the final string, minus any leading/trailing whitespace.
+    return clean.trim();
+}
+
+/**
+ * Returns the content of a news article via URL.  Text is extracted using a method similar to readability.
+ * @param url
+ * @param callback
+ */
+function getArticleContent(url, callback) {
+    readability(url, (err, article, response) => {
+        if (err) return callback(err);
+
+        // ** Ensure we have a valid response status code
+        if (response.statusCode !== 200)
+            return callback(errors('INVALID_STATUS_CODE', {statusCode: response.statusCode, url: url}));
+
+        // const $ = cheerio.load(article);
+
+        //logger.info(article);
+
+        var obj = {
+            "url": url,
+            "title": article.getTitle().trim(),
+            "contents": stripHTML(article.getContent() || "")
+        };
+
+        callback(null, obj);
+    });
+}
+
+function getUrlEntities(url, callback) {
+    if (!url)
+        url = "http://www.hollywoodreporter.com/news/box-office-spectre-stays-no-840163";
+
+    getArticleContent(url, (err, content) => {
+        if (err) return callback(err);
+
+
+    });
+}
+
 module.exports.getTopics = getTopics;
 module.exports.getHeadlines = getHeadlines;
+module.exports.getEntities = getEntities;
+module.exports.getUrlEntities = getUrlEntities;
